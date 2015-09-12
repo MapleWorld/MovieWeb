cc85adf0420749c1804ac332049eb513f2fcda0c
MapleWorld <ou.ye.canada@hotmail.com>
2015-09-12 14:52:40 -0400

    Updated create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -1,10 +1,7 @@
--- Tested with MySQL v5.1.73
+-- EXECUTED THIS FIRST
+CREATE SCHEMA movies;
 
--- CREATE SCHEMA movies;
-
--- insert into user (username, password, created_date, email, mailing_address, phone, gender, money, organization, interest, admin) values 
---	('admin','111111', '2014-10-23 20:04:25', 'test1@email.com', 'toronto', '416-888-8888', 'male', 999999, 'Uoft', 'health', 1); 
-	
+-- EXECUTED THIS INSIDE movies AFTER YOU CREATED THE SCHEMA 
 create table movie (
 	-- General Data
 	id int AUTO_INCREMENT PRIMARY KEY,
